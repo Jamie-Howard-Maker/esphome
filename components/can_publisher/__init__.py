8d811c3d450226d7d45d1b065f7a45a25fba96f0
Jamie-Howard-Maker <jamiehoward430@gmail.com>
2026-09-16 13:07:31 +0100

    Implement CANPublisher configuration and code generation
    Added configuration schema and to_code function for CANPublisher.

--- components/can_publisher/__init__.py
+++ components/can_publisher/__init__.py
@@ -1,1 +1,14 @@
-from .can_publisher import CANPublisher
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.const import CONF_ID
+
+can_publisher_ns = cg.esphome_ns.namespace("can_publisher")
+CANPublisher = can_publisher_ns.class_("CANPublisher", cg.Component)
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(CANPublisher),
+}).extend(cv.COMPONENT_SCHEMA)
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
